94950bd32b53cdf3f4a2137501f6c75ee447e074
Mr-Donot <florian.daunay37@gmail.com>
2023-10-31 15:11:00 +0100

    Refacto First map

--- map.py
+++ map.py
@@ -5,22 +5,23 @@ from block import *
 def generate_map():
     blocks = []
 
-    blocks.append(Block(0, SCREEN_HEIGHT-20, SCREEN_WIDTH, 20, Color.GREEN.value)) #ground
+    blocks.append(Block(0, SCREEN_HEIGHT-SCREEN_HEIGHT/30, SCREEN_WIDTH, SCREEN_HEIGHT/30, Color.GREEN.value)) #ground
 
 
-    blocks.append(Block(SCREEN_WIDTH*3/16, SCREEN_HEIGHT*13/16, SCREEN_WIDTH/8, 20, Color.BLUE.value))
-    blocks.append(Block(SCREEN_WIDTH/2 - SCREEN_WIDTH/8, SCREEN_HEIGHT*11/16, SCREEN_WIDTH/8, 20, Color.BLUE.value))
-    blocks.append(Block(SCREEN_WIDTH*3/4 - SCREEN_WIDTH/8, SCREEN_HEIGHT*9/16, SCREEN_WIDTH/8, 20, Color.BLUE.value))
+    blocks.append(Block(SCREEN_WIDTH*3/16, SCREEN_HEIGHT*13/16, SCREEN_WIDTH/8, SCREEN_HEIGHT/30, Color.BLUE.value))
+    blocks.append(Block(SCREEN_WIDTH/2 - SCREEN_WIDTH/8, SCREEN_HEIGHT*11/16, SCREEN_WIDTH/8, SCREEN_HEIGHT/30, Color.BLUE.value))
+    blocks.append(Block(SCREEN_WIDTH*3/4 - SCREEN_WIDTH/8, SCREEN_HEIGHT*9/16, SCREEN_WIDTH/8, SCREEN_HEIGHT/30, Color.BLUE.value))
+    blocks.append(Block(SCREEN_WIDTH*11/32, SCREEN_HEIGHT*15/32, SCREEN_WIDTH/8, SCREEN_HEIGHT/30, Color.BLUE.value))
     
-    blocks.append(Block(SCREEN_WIDTH*11/32, SCREEN_HEIGHT*15/32, SCREEN_WIDTH/8, 20, Color.BLUE.value))
-    
-    blocks.append(Block(SCREEN_WIDTH*7/32, SCREEN_HEIGHT*5/16, SCREEN_WIDTH/8, 20, Color.BLUE.value))
-    blocks.append(Block(SCREEN_WIDTH*7/16, SCREEN_HEIGHT*3/16, SCREEN_WIDTH/8, 20, Color.BLUE.value))
-    blocks.append(Block(SCREEN_WIDTH*21/32, SCREEN_HEIGHT*9/32, SCREEN_WIDTH/8, 20, Color.BLUE.value))
+    blocks.append(Block(SCREEN_WIDTH*4/64, SCREEN_HEIGHT*12/32, SCREEN_WIDTH/8, SCREEN_HEIGHT/30, Color.BLUE.value))
+    blocks.append(Block(SCREEN_WIDTH*7/32, SCREEN_HEIGHT*8/32, SCREEN_WIDTH/8, SCREEN_HEIGHT/30, Color.BLUE.value))
+
+    blocks.append(Block(SCREEN_WIDTH*7/16, SCREEN_HEIGHT*3/16, SCREEN_WIDTH/8, SCREEN_HEIGHT/30, Color.BLUE.value))
+    blocks.append(Block(SCREEN_WIDTH*43/64, SCREEN_HEIGHT*9/32, SCREEN_WIDTH/8, SCREEN_HEIGHT/30, Color.BLUE.value))
 
 
 
 
-    blocks.append(Block(SCREEN_WIDTH*13/16, SCREEN_HEIGHT*3/16, 30, 20, Color.YELLOW.value, type="win"))
+    blocks.append(Block(SCREEN_WIDTH*7/8, SCREEN_HEIGHT*3/16, SCREEN_WIDTH*3/80, SCREEN_HEIGHT/30, Color.YELLOW.value, type="win"))
 
     return blocks
